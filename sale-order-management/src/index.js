082c1fe02c44f32a8d71a98c512f15ad5d341062
prachik2407 <109075683+prachik2407@users.noreply.github.com>
2024-06-12 12:09:12 +0530

    update

--- sale-order-management/src/index.js
+++ sale-order-management/src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import theme from './theme';
+import reportWebVitals from './reportWebVitals';
 
-const queryClient = new QueryClient();
-
-ReactDOM.render(
-  <ChakraProvider>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-    </QueryClientProvider>
-  </ChakraProvider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
